701f5319755e9f40cdf88b15b66fef9c0fef453a
siddhant <sjawa@usc.edu>
2016-01-27 23:31:00 -0800

    Change directory structure

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+flask_app = Flask(__name__)  # Always in this order otherwise app won't be imported
+
+# import sys
+# sys.path.append('/home/siddhant/PycharmProjects/DogShelter/myapp/app')
+
+# sys.path.append('/home/siddhant/PycharmProjects/DogShelter/myapp/app/Shelter')
+
+# sys.path.append('/home/siddhant/PycharmProjects/DogShelter/myapp/app/Puppy')
+
+
+import app.main
---
+++ app/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from flask import render_template
+from app import flask_app
+
+
+@flask_app.route('/')
+@flask_app.route('/index.html')
+def welcome_page():
+    # pdb.set_trace()
+    return render_template('index.html')
+
+
